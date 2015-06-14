c1df6e65ce2f0f7662a13d4059bc25418a9f806c
Aaron Bonnell-Kangas <abk@aaronbeekay.info>
2015-06-14 16:58:11 -0400

    hack up m_status to read BMS values -- does not currently comply with API, needs fixed

--- var/www/html/Website/m_assets/js/m_status.js
+++ var/www/html/Website/m_assets/js/m_status.js
@@ -1,193 +1,69 @@
 $( document ).ready(function() {
-	serverHost = '192.168.7.35';
+	serverHost = '192.168.4.1';
 	serverPort = '80';
+	
   	// automatically update system status
   	updateAll();
   	
   	setInterval(function() {
   		updateAll();
-  		updateVehicleStatus();
   	}, 1000);
 } );
 
-function updateMessage( messageName ){
-	// send GET request to update all status vars
-	$.getJSON('http://' + serverHost + ':' + serverPort + '/api/message.php?messageName=' + messageName, 
-		function(data) { 
-		
-			varName = data.messageName;
-			value = data.messageValue;
-			status = data.status;
-			
-			$(".system-variable-value#"+varName).text( value );
-			
-			switch( status ){
-				case "Warn":
-					$(".system-variable-name#"+varName).css( 'color', 'yellow' );
-					break;
-					
-				case "Fail":
-					$(".system-variable-name#"+varName).css( 'color', 'red' );
-					break;
-					
-				default:
-					$(".system-variable-name#"+varName).css( 'color', '' );
-					break;
-			}
-		
-		} );
-}
-
-function updateVehicleStatus( ){
-	// Update the vehicle status variables that trigger popovers (frame fault, HV state, E-stop)
-	$.getJSON('http://' + serverHost + ':' + serverPort + '/api/vehicleStatus.php', 
-		function(data) { 
-			if( data.highVoltage == "True" ){
-				$(".alert-hv").removeClass("hidden");
-			} else {
-				$(".alert-hv").addClass("hidden");
-			}
-			
-			if( data.FrameFault == "True"){
-				$(".alert-framefault").removeClass("hidden");
-			} else {
-				$(".alert-framefault").addClass("hidden");
-			}
-			
-			if( data.EStop == "True"){
-				$(".alert-estop").removeClass("hidden");
-			} else {
-				$(".alert-estop").addClass("hidden");
-			}
-	} );
-}
-
 function updateAll( ){
 	// send GET request to update all status vars
-	$.getJSON('http://' + serverHost + ':' + serverPort + '/api/system.php?systemName=powertrain', 
-		function(data) { 
-			for(var i = 0; i < data.length; i++){
-				varName = data[i].messageName;
-				value 	= data[i].messageValue;
-				status 	= data[i].messageStatus;
+	uri = 'http://' + serverHost + ':' + serverPort + '/reload_bms.php';
 	
-				$(".system-variable-value#"+varName).text( value );
-				
-				var yellow = "rgb(240, 173, 78)";
-			
-				switch( status ){
-					case "Warn":
-						$(".system-variable-name#"+varName).css( 'color', yellow );
-						break;
-					
-					case "Fail":
-						$(".system-variable-name#"+varName).css( 'color', 'red' );
-						break;
-					
-					default:
-						$(".system-variable-name#"+varName).css( 'color', '' );
-						break;
-				}
-			}
-			
-			// UGLY HACK FOR SYSTEM STATUS DETERMINATION
-			// motor controller
-			if( $(".system-variable-name#PhaseAtemp").css( 'color' ) 	== "rgb(255, 0, 0)" || /* red */
-				$(".system-variable-name#BusVoltage").css( 'color' ) 	== "rgb(255, 0, 0)" ||
-				$(".system-variable-name#MotorId").css( 'color' ) 		== "rgb(255, 0, 0)" ) {
-				
-				// set parent container to red
-				$("div#mc_temperature").parents(".bs-callout").removeClass("bs-callout-success");
-				$("div#mc_temperature").parents(".bs-callout").removeClass("bs-callout-warning");
-				$("div#mc_temperature").parents(".bs-callout").addClass("bs-callout-danger");
-				
-			} else if( $(".system-variable-name#PhaseAtemp").css( 'color' ) 	== yellow || /* yellow */
-				$(".system-variable-name#BusVoltage").css( 'color' ) 		== yellow ||
-				$(".system-variable-name#MotorId").css( 'color' ) 			== yellow ) {
-				
-				// set parent container to yellow
-				$("div#mc_temperature").parents(".bs-callout").removeClass("bs-callout-success");
-				$("div#mc_temperature").parents(".bs-callout").removeClass("bs-callout-danger");
-				$("div#mc_temperature").parents(".bs-callout").addClass("bs-callout-warning");
-				
+	var minCellVoltage, maxCellVoltage = 0;
+	
+	var jqxhr = $.getJSON(uri, 
+		function(data) { 
+			if( $("#ignore-bim-3").is(':checked') ){
+				var minVoltages = [ data["BIM1MinCellVoltage"],
+									data["BIM2MinCellVoltage"],
+									5000,
+									data["BIM4MinCellVoltage"],
+									data["BIM5MinCellVoltage"] 	];
+									
+				var maxVoltages = [ data["BIM1MaxCellVoltage"],
+									data["BIM2MaxCellVoltage"],
+									0,
+									data["BIM4MaxCellVoltage"],
+									data["BIM5MaxCellVoltage"] 	];
+									
 			} else {
-				
-				// set parent container to green
-				$("div#mc_temperature").parents(".bs-callout").removeClass("bs-callout-warning");
-				$("div#mc_temperature").parents(".bs-callout").removeClass("bs-callout-danger");
-				$("div#mc_temperature").parents(".bs-callout").addClass("bs-callout-success");
+				var minVoltages = [ data["BIM1MinCellVoltage"],
+									data["BIM2MinCellVoltage"],
+									data["BIM3MaxCellVoltage"],
+									data["BIM4MinCellVoltage"],
+									data["BIM5MinCellVoltage"] 	];					
+				var maxVoltages = [ data["BIM1MaxCellVoltage"],
+									data["BIM2MaxCellVoltage"],
+									data["BIM3MaxCellVoltage"],
+									data["BIM4MaxCellVoltage"],
+									data["BIM5MaxCellVoltage"] 	];
 			}
+									
+				var minCellVoltage 	= Math.min( minVoltages );		
+				var maxCellVoltage 	= Math.max( maxVoltages );				
+				var minPack 		= minVoltages.indexOf( Math.min.apply(Math, minVoltages) ) + 1;
+				var maxPack 		= maxVoltages.indexOf( Math.max.apply(Math, maxVoltages) ) + 1;
+				
+			$(".system-variable-value#pack_highcell").text( maxCellVoltage/1000 );
+			$(".system-variable-value#pack_lowcell").text( minCellVoltage/1000 );
+			$(".system-variable-value#pack_lowcell_packnum").text( minPack );
+			$(".system-variable-value#pack_highcell_packnum").text( maxPack);
 			
-			// motor
-			if( $(".system-variable-name#MotorTemp").css( 'color' ) 	== "rgb(255, 0, 0)" || /* red */
-				$(".system-variable-name#MotorVelocity").css( 'color' ) == "rgb(255, 0, 0)" ) {
-				
-				// set parent container to red
-				$("div#motor_MotorTemp").parents(".bs-callout").removeClass("bs-callout-success");
-				$("div#motor_MotorTemp").parents(".bs-callout").removeClass("bs-callout-warning");
-				$("div#motor_MotorTemp").parents(".bs-callout").addClass("bs-callout-danger");
-				
-			} else if( $(".system-variable-name#MotorTemp").css( 'color' ) 	== yellow || /* yellow */
-				$(".system-variable-name#MotorVelocity").css( 'color' ) 		== yellow ) {
-				
-				// set parent container to yellow
-				$("div#motor_MotorTemp").parents(".bs-callout").removeClass("bs-callout-success");
-				$("div#motor_MotorTemp").parents(".bs-callout").removeClass("bs-callout-danger");
-				$("div#motor_MotorTemp").parents(".bs-callout").addClass("bs-callout-warning");
-				
-			} else {
-				
-				// set parent container to green
-				$("div#motor_MotorTemp").parents(".bs-callout").removeClass("bs-callout-warning");
-				$("div#motor_MotorTemp").parents(".bs-callout").removeClass("bs-callout-danger");
-				$("div#motor_MotorTemp").parents(".bs-callout").addClass("bs-callout-success");
 			}
-			
-			// pack
-			if( $(".system-variable-name#PackTemp").css( 'color' ) 		== "rgb(255, 0, 0)" || /* red */
-				$(".system-variable-name#PackSOC").css( 'color' ) 		== "rgb(255, 0, 0)" ||
-				$(".system-variable-name#PackBalance").css( 'color' ) 	== "rgb(255, 0, 0)" ) {
-				
-				// set parent container to red
-				$("div#pack_temperature").parents(".bs-callout").removeClass("bs-callout-success");
-				$("div#pack_temperature").parents(".bs-callout").removeClass("bs-callout-warning");
-				$("div#pack_temperature").parents(".bs-callout").addClass("bs-callout-danger");
-				
-			} else if( $(".system-variable-name#PackTemp").css( 'color' ) 		== yellow || /* yellow */
-				$(".system-variable-name#PackSOC").css( 'color' ) 				== yellow ||
-				$(".system-variable-name#PackBalance").css( 'color' ) 			== yellow ) {
-				
-				// set parent container to yellow
-				$("div#pack_temperature").parents(".bs-callout").removeClass("bs-callout-success");
-				$("div#pack_temperature").parents(".bs-callout").removeClass("bs-callout-danger");
-				$("div#pack_temperature").parents(".bs-callout").addClass("bs-callout-warning");
-				
-			} else {
-				
-				// set parent container to green
-				$("div#pack_temperature").parents(".bs-callout").removeClass("bs-callout-warning");
-				$("div#pack_temperature").parents(".bs-callout").removeClass("bs-callout-danger");
-				$("div#pack_temperature").parents(".bs-callout").addClass("bs-callout-success");
-			}
-		} );
-}
-
-function setSystemStatus( system, status ){
-	system = system.toLowerCase();
-	switch( status ){
-		case "OK":
-			$(".system-status-indicator#" + system).css("background-color", "green");
-			break;
-		case "Warn":
-			$(".system-status-indicator#" + system).css("background-color", "yellow");
-			break;
-		case "Fail":
-			$(".system-status-indicator#" + system).css("background-color", "red");
-			break;
-		case "NoData":
-			$(".system-status-indicator#" + system).css("background-color", "gray");
-			// hmm what else should we do here
-		default:
-			console.log("updateSystemStatus() called without a valid status");
+		)
+		.error( 	
+			function(jqxhr, textStatus, error) {
+				var err = textStatus + ", " + error;
+				console.log("failure: " + err);
+			 }
+		);
 	}
-}
+
+/*
+
+*/
